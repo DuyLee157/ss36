98b709d027700badcb1c08b7c71f7ef8dac5d6cf
DuyLee157 <148028094+DuyLee157@users.noreply.github.com>
2024-01-25 10:13:25 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,7 @@
+function add_item(){
+    let kt = document.getElementById("check_answer")
+    if(kt.length < 10 ){
+        document.write("khong hop lệ")
+        } else { document.write(" hop le")
+    }
+}
